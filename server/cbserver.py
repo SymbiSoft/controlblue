596fa3caf77380b6c3412e6d74333cd8ac8457af
HellFeuer <HellFeuer@c3af2635-f62b-0410-9653-c3c7c586ac3e>
2007-03-19 16:21:23 +0000

    Basic authentication added, some cleanup

--- server/cbserver.py
+++ server/cbserver.py
@@ -1,6 +1,9 @@
 from lightblue import *
 import os
 
+trusted = []
+passwd = u"p"
+
 s = socket()
 s.bind(("", 0))
 s.listen(1)
@@ -9,6 +12,13 @@ data = ""
 while data != "exit":
   conn, addr = s.accept()
   data = conn.recv(1024)
+  if (addr in trusted):
+    (data != "exit") and os.system(data)
+    conn.send(u'1') #3 is sent if operation is not allowed
+  elif ((data[0:4] == u'pass') and (data[5:] == passwd)):
+    trusted.append(addr)
+    conn.send(u"1")
+  else:
+    conn.send(u"0")
   conn.close()
-  (data != "exit") and os.system(data) 
 s.close()
