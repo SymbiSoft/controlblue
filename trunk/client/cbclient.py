d44d67a11b67cfa3ae5dc485b13eb28fa68a0c6e
HellFeuer <HellFeuer@c3af2635-f62b-0410-9653-c3c7c586ac3e>
2007-03-19 16:21:23 +0000

    Basic authentication added, some cleanup

--- trunk/client/cbclient.py
+++ trunk/client/cbclient.py
@@ -2,26 +2,49 @@ import e32, socket, appuifw
 
 CONFIG_DIR = 'e:/controlblue'
 CONFIG_FILE=os.path.join(CONFIG_DIR, 'commandlist.txt')
+SERVICE_NAME = "controlblue"
 
 class BlueToothError(Exception):
   def __init__(self, value):
     self.value = value
+    appuifw.note(value, 'error')
   def __str__(self):
     return repr(self.value)
 
 class Computer:
-  def __init__(self, servicename):
+  def __init__(self):
+    self.address = None
+    self.discover()
+
+  def discover(self):
     try:
       address,services = socket.bt_discover()
     except:
-      raise BlueToothError("No services found")
+      self.address = None
+      raise BlueToothError(u'Not connected')
+    if unicode(SERVICE_NAME) in services:
+      self.address = address	
+      self.portno = services[unicode(SERVICE_NAME)]
+      self.auth() or appuifw.note(u'Authentication failed!', 'error')
     else:
-      if unicode(servicename) in services:
-	self.address = address
-	self.portno = services[unicode(servicename)]
-      else:
-	raise BlueToothError("No services found")
+      appuifw.note(u'shit')
+      self.address = None
+      raise BlueToothError(SERVICE_NAME + u' is not running on selected device')
 
+  def auth(self):
+    appuifw.note(u'noshit')
+    if (not self.address):
+      appuifw.note (u'Not connected!', 'error')
+      return False
+    passwd = appuifw.query(u'Authentication required. Enter password', 'code')
+    while (passwd):
+      result = self.send(u"pass " + passwd)
+      if (result == u'0'):
+	passwd =  appuifw.query(u'Incorrect password! Try again', 'code')
+      else:
+	return True
+    return False
+  
   def connect(self):
     self.s = socket.socket(socket.AF_BT, socket.SOCK_STREAM)
     self.s.connect((self.address, self.portno))
@@ -29,7 +52,25 @@ class Computer:
   def send(self, data):
     self.connect()
     self.s.send(unicode(data))
+    result = self.s.recv(4)
     self.close()
+    return result
+
+  def sendCommand(self, data):
+    if (not self.address):
+      try:
+	self.discover()
+      except BlueToothError, inst:
+	return
+    result = self.send(data)
+    if (result == u'0'):
+      if (self.auth()):
+	self.send(data)
+      else:
+	appuifw.note(u'Authentication failed!', 'error')
+	return
+    if (result == u'3'):
+      appuifw.note(u'This operation is not allowed!', 'error')
 
   def close(self):
     self.s.close()
@@ -37,9 +78,6 @@ class Computer:
 class commandList:
   commands = [u"Empty..."]
   isEmpty = True
-
-  def __init__(self, computer):
-    self.computer = computer
   
   def read(self):
     try:
@@ -74,7 +112,7 @@ class commandList:
     if (self.isEmpty):
       return
     index = self.lb.current()
-    self.computer.send(self.commands[index])
+    mycomputer and mycomputer.sendCommand(self.commands[index])
 
   def newCmd(self):
     data = appuifw.query(u"Enter command", "text")
@@ -95,20 +133,25 @@ class commandList:
       return
     cmd = self.commands[self.lb.current()]
     cmd = appuifw.query(u"Edit Command:", 'text', cmd)
+    
 
 def exit_key_handler():
   mycommands.write()
   app_lock.signal()
 
 try:
-  mycommands = commandList(Computer("controlblue"))
-  appuifw.app.screen = 'normal'
-  appuifw.app.title = u"controlBlue"
-  mycommands.read()
-  appuifw.app.body = mycommands.lb
-  appuifw.app.exit_key_handler = exit_key_handler
-  appuifw.app.menu = [(u'New', mycommands.newCmd), (u'Edit', mycommands.editCmd), (u'Delete', mycommands.remCmd)]
-  app_lock = e32.Ao_lock()
-  app_lock.wait()
-except BlueToothError, inst:
-  print inst.value
+  mycomputer = Computer()
+except BlueToothError:
+  pass
+mycommands = commandList()
+
+MENU_LIST = [(u'New', mycommands.newCmd), (u'Edit', mycommands.editCmd), (u'Delete', mycommands.remCmd), (u'Connection', ((u'Reconnect', mycomputer.discover), (u'Authenticate', mycomputer.auth)))]
+
+appuifw.app.screen = 'normal'
+appuifw.app.title = u"controlBlue"
+mycommands.read()
+appuifw.app.body = mycommands.lb
+appuifw.app.exit_key_handler = exit_key_handler
+appuifw.app.menu = MENU_LIST
+app_lock = e32.Ao_lock()
+app_lock.wait()
