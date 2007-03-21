248b6cd5509ce3f0167904ae08db8a5f9e8b8013
HellFeuer <HellFeuer@c3af2635-f62b-0410-9653-c3c7c586ac3e>
2007-03-21 08:14:31 +0000

    Removed lightblue dependancy

--- server/cbserver.py
+++ server/cbserver.py
@@ -1,14 +1,15 @@
-from lightblue import *
 import bluetooth
 import os
 
 trusted = []
 passwd = u"p"
 
-s = socket()
-s.bind(("", 0))
+s = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+port = bluetooth.get_available_port( bluetooth.RFCOMM )
+s.bind(("",port))
 s.listen(1)
-advertise("controlblue", s, RFCOMM)
+uuid = "1e0ca4ea-299d-4335-93eb-27fcfe7fa848"
+bluetooth.advertise_service(s, "controlblue", uuid,[bluetooth.SERIAL_PORT_CLASS],[bluetooth.SERIAL_PORT_PROFILE])
 data = ""
 while data != "exit":
   conn, addr = s.accept()
